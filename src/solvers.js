35a9dbd144d430e151acd474e350a6fa04c3e262
willsimmons <willsimmons@gmail.com>
2016-09-22 12:25:17 -0700

    Implement findnRooksSolution solver method

--- src/solvers.js
+++ src/solvers.js
@@ -14,60 +14,23 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
+//we just need a single solution...
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
 
   var board = new Board({n: n});
-  var noneConflictsTupple = [];
 
-  var verifyPieces = function(row, column) {
-    // These two for loops for the first piece
-    for (var a = 0; a < board.rows().length; a++) {
-      for (var b = 0; b < board.rows()[a].length; b++) {
-        board.togglePiece(a, b);
-        // These two for loops for the second piece
-        for (var i = 0; i < board.rows().length; i++) {
-          for (var j = 0; j < board.rows()[i].length; j++) {
-            if (board.rows()[i][j] === 1) {
-              continue;
-            }
-            board.togglePiece(i, j);
-            if (board.hasAnyRooksConflicts()) {
-              board.togglePiece(i, j);
-            } else {
-              if (a === i && b === j) {
-                continue;
-              }
-              noneConflictsTupple.push([[a, b], [i, j]]);
-            }
-          }
-        }
-
-        for (var i = 0; i < board.rows().length; i++) {
-          for (var j = 0; j < board.rows()[i].length; j++) {
-            if (board.rows()[i][j] === 1) {
-              continue;
-            }
-            board.togglePiece(i, j);
-            if (board.hasAnyRooksConflicts()) {
-              board.togglePiece(i, j);
-            } else {
-              if (a === i && b === j) {
-                continue;
-              }
-              noneConflictsTupple.push([[a, b], [i, j]]);
-            }
-          }
-        }
-
-        board.togglePiece(a, b);
-      }
+  if (n === 1) {
+    solution.push([1]);
+  } else {
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(i, i);
+    } 
+  
+    if (!board.hasAnyRooksConflicts()) {
+      solution = board.rows().slice();
     }
-  };
-
-  debugger;
-  verifyPieces(0, 0);
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
